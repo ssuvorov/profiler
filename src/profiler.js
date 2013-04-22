f9d1e29d2c4de6dabf9c9f69828c20227d470b4b
dmitryshimkin <dmitryshimkin@gmail.com>
2013-04-23 02:18:34 +0400

    Initial files

---
+++ src/profiler.js
@@ -0,0 +1,44 @@
+var start = profiler.start.valueOf();
+
+window.profiler = {
+	/**
+	 * @public
+	 * TBD
+	 */
+	start: function () {
+		//
+	},
+
+	/**
+	 * @public
+	 * TBD
+	 */
+	end: function () {
+		//
+	},
+
+	/**
+	 * @public
+	 * TBD
+	 */
+	report: function () {
+		//
+	},
+
+
+	/**
+	 * @public
+	 * TBD
+	 */
+	reset: function () {
+		//
+	},
+
+	/**
+	 * @public
+	 * TBD
+	 */
+	send: function () {
+		//
+	}
+};
