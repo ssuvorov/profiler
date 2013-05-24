fc08399bfb72dba3706e5a48e53cb94f07f74433
Dmitry Shimkin <d.shimkin@corp.mail.ru>
2013-05-24 14:00:39 +0400

    tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 module.exports = function(grunt) {
   'use strict';
 
-	grunt.initConfig({
-		clean: {
-			test: ['build/profiler.test.js']
-		},
+  grunt.initConfig({
+    clean: {
+      test: ['build/profiler.test.js']
+    },
 
-		concat: {
+    concat: {
       build: {
         files: {
           'build/profiler.js': [
@@ -21,41 +21,41 @@ module.exports = function(grunt) {
           ]
         }
       },
-			test: {
-				files: {
-					'build/profiler.test.js': [
-						'src/lang/typeof.js',
-						'src/lang/each.js',
-						'src/lang/filter.js',
-						'src/dom/event.js',
+      test: {
+        files: {
+          'build/profiler.test.js': [
+            'src/lang/typeof.js',
+            'src/lang/each.js',
+            'src/lang/filter.js',
+            'src/dom/event.js',
             'src/provider/http.js',
-						'src/start.js',
-						'src/record.js',
-						'src/profiler.js'
-					]
-				}
-			}
-		},
+            'src/start.js',
+            'src/record.js',
+            'src/profiler.js'
+          ]
+        }
+      }
+    },
 
-		jasmine: {
-			dev: {
-				src: 'build/profiler.js',
-				options: {
-					specs: 'test/specs/profiler.spec.js',
-					template: require('grunt-template-jasmine-istanbul'),
-					templateOptions: {
-						coverage: 'test/report/coverage.json',
-						report: [
-							{
-								type: 'html',
-								options: {
-									dir: 'test/report/html'
-								}
-							}
-						]
-					}
-				}
-			},
+    jasmine: {
+      dev: {
+        src: 'build/profiler.js',
+        options: {
+          specs: 'test/specs/profiler.spec.js',
+          template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+            coverage: 'test/report/coverage.json',
+            report: [
+              {
+                type: 'html',
+                options: {
+                  dir: 'test/report/html'
+                }
+              }
+            ]
+          }
+        }
+      },
       prod: {
         src: 'build/profiler.min.js',
         options: {
@@ -74,7 +74,7 @@ module.exports = function(grunt) {
           }
         }
       }
-		},
+    },
 
     jshint: {
       alldev: [
@@ -108,21 +108,21 @@ module.exports = function(grunt) {
         ]
       }
     }
-	});
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks('grunt-wrap');
 
-	grunt.registerTask('build-dev', ['concat:build', 'wrap:all']);
-	grunt.registerTask('build-prod', ['build-dev', 'uglify:prod']);
-	grunt.registerTask('test-dev', ['build-dev', 'jasmine:dev']);
-	grunt.registerTask('test-prod', ['build-prod', 'jasmine:prod']);
+  grunt.registerTask('build-dev', ['concat:build', 'wrap:all']);
+  grunt.registerTask('build-prod', ['build-dev', 'uglify:prod']);
+  grunt.registerTask('test-dev', ['build-dev', 'jasmine:dev']);
+  grunt.registerTask('test-prod', ['build-prod', 'jasmine:prod']);
 
-	grunt.registerTask('test', ['test-dev']);
-	grunt.registerTask('prod', ['build-prod']);
-	grunt.registerTask('hint', ['build-dev', 'jshint']);
-	grunt.registerTask('default', ['build-dev']);
+  grunt.registerTask('test', ['test-dev']);
+  grunt.registerTask('prod', ['build-prod']);
+  grunt.registerTask('hint', ['build-dev', 'jshint']);
+  grunt.registerTask('default', ['build-dev']);
 };
